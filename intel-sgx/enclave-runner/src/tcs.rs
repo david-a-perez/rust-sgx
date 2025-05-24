4074c12eeee820e2f61a471e01a7eb335db2ec1b
David Perez <david.a.perez@vanderbilt.edu>
2025-05-24 19:06:51 -0400

    Conditionally compile linux code

--- intel-sgx/enclave-runner/src/tcs.rs
+++ intel-sgx/enclave-runner/src/tcs.rs
@@ -6,9 +6,8 @@
 
 use std::arch::asm;
 use std::cell::RefCell;
-use std::convert::{TryFrom, TryInto};
-use std::fmt;
-use std::io::Write;
+#[cfg(target_os = "linux")]
+use std::{convert::{TryFrom, TryInto}, fmt, io::Write};
 use std::os::raw::c_void;
 
 use sgx_isa::Enclu;
@@ -103,85 +102,91 @@ pub(crate) fn coenter<T: Tcs>(
             }
         };
         if has_vdso_sgx_enter_enclave() {
-            #[repr(C)]
-            #[derive(Default)]
-            struct SgxEnclaveRun {
-                tcs: u64,
-                function: u32,
-                exception_vector: u16,
-                exception_error_code: u16,
-                exception_addr: u64,
-                user_handler: u64,
-                user_data: u64,
-                reserved: [u64; 27],
-            }
+            #[cfg(target_os = "linux")]
+            {
+                #[repr(C)]
+                #[derive(Default)]
+                struct SgxEnclaveRun {
+                    tcs: u64,
+                    function: u32,
+                    exception_vector: u16,
+                    exception_error_code: u16,
+                    exception_addr: u64,
+                    user_handler: u64,
+                    user_data: u64,
+                    reserved: [u64; 27],
+                }
 
-            impl fmt::Debug for SgxEnclaveRun {
-                fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                    let function = Enclu::try_from(self.function);
-                    let fundbg = match &function {
-                        Ok(v) => v as &dyn fmt::Debug,
-                        Err(_) => &self.function,
-                    };
-                    f.debug_struct("SgxEnclaveRun")
-                        .field("function", fundbg)
-                        .field("exception_vector", &self.exception_vector)
-                        .field("exception_error_code", &self.exception_error_code)
-                        .field("exception_addr", &(self.exception_addr as *mut ()))
-                        .finish()
+                impl fmt::Debug for SgxEnclaveRun {
+                    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                        let function = Enclu::try_from(self.function);
+                        let fundbg = match &function {
+                            Ok(v) => v as &dyn fmt::Debug,
+                            Err(_) => &self.function,
+                        };
+                        f.debug_struct("SgxEnclaveRun")
+                            .field("function", fundbg)
+                            .field("exception_vector", &self.exception_vector)
+                            .field("exception_error_code", &self.exception_error_code)
+                            .field("exception_addr", &(self.exception_addr as *mut ()))
+                            .finish()
+                    }
+                }
+
+                let mut run = SgxEnclaveRun {
+                    tcs: tcs.address() as _,
+                    ..Default::default()
+                };
+                let ret: i32;
+                asm!("
+                        sub $0x8, %rsp                   // align stack
+                        push {}                          // push argument: run
+    .weak __vdso_sgx_enter_enclave
+    .type __vdso_sgx_enter_enclave, function
+                        call __vdso_sgx_enter_enclave@PLT
+                        add $0x10, %rsp                  // restore stack pointer
+                    ",
+                    in(reg) &mut run,
+                    lateout("eax") ret,
+                    /* rbx unused */
+                    inout("rcx") Enclu::EEnter as u64 => _,
+                    inout("rdx") p3,
+                    inout("rdi") p1,
+                    inout("rsi") p2,
+                    inout("r8") p4,
+                    inout("r9") p5,
+                    inout("r10") debug_buf_ptr => _,
+                    lateout("r11") _,
+                    lateout("r12") _, // these may be clobbered in case of AEX
+                    lateout("r13") _, // V
+                    lateout("r14") _, // V
+                    lateout("r15") _, // V
+                    options(att_syntax)
+                );
+                if ret == 0 {
+                    sgx_result = run.function;
+                    match sgx_result.try_into() {
+                        Ok(Enclu::EExit) => { /* normal case */ },
+                        Ok(Enclu::EResume) => {
+                            if let Some(mut debug_buf) = debug_buf {
+                                let _ = write!(&mut debug_buf[..], "Enclave triggered exception: {:?}\0", run);
+                            } else {
+                                eprintln!("Enclave triggered exception, treating as panic: {:?}", run);
+                            }
+                            return CoResult::Yield(Usercall {
+                                tcs: tcs,
+                                parameters: (crate::usercalls::abi::UsercallList::exit as _, true as _, 0, 0, 0),
+                            });
+                        },
+                        _ => panic!("Error entering enclave (VDSO): ret = success, run = {:?}", run),
+                    }
+                } else {
+                    panic!("Error entering enclave (VDSO): ret = {:?}, run = {:?}", std::io::Error::from_raw_os_error(-ret), run);
                 }
             }
 
-            let mut run = SgxEnclaveRun {
-                tcs: tcs.address() as _,
-                ..Default::default()
-            };
-            let ret: i32;
-            asm!("
-                    sub $0x8, %rsp                   // align stack
-                    push {}                          // push argument: run
-.weak __vdso_sgx_enter_enclave
-.type __vdso_sgx_enter_enclave, function
-                    call __vdso_sgx_enter_enclave@PLT
-                    add $0x10, %rsp                  // restore stack pointer
-                ",
-                in(reg) &mut run,
-                lateout("eax") ret,
-                /* rbx unused */
-                inout("rcx") Enclu::EEnter as u64 => _,
-                inout("rdx") p3,
-                inout("rdi") p1,
-                inout("rsi") p2,
-                inout("r8") p4,
-                inout("r9") p5,
-                inout("r10") debug_buf_ptr => _,
-                lateout("r11") _,
-                lateout("r12") _, // these may be clobbered in case of AEX
-                lateout("r13") _, // V
-                lateout("r14") _, // V
-                lateout("r15") _, // V
-                options(att_syntax)
-            );
-            if ret == 0 {
-                sgx_result = run.function;
-                match sgx_result.try_into() {
-                    Ok(Enclu::EExit) => { /* normal case */ },
-                    Ok(Enclu::EResume) => {
-                        if let Some(mut debug_buf) = debug_buf {
-                            let _ = write!(&mut debug_buf[..], "Enclave triggered exception: {:?}\0", run);
-                        } else {
-                            eprintln!("Enclave triggered exception, treating as panic: {:?}", run);
-                        }
-                        return CoResult::Yield(Usercall {
-                            tcs: tcs,
-                            parameters: (crate::usercalls::abi::UsercallList::exit as _, true as _, 0, 0, 0),
-                        });
-                    },
-                    _ => panic!("Error entering enclave (VDSO): ret = success, run = {:?}", run),
-                }
-            } else {
-                panic!("Error entering enclave (VDSO): ret = {:?}, run = {:?}", std::io::Error::from_raw_os_error(-ret), run);
-            }
+            #[cfg(not(target_os = "linux"))]
+            unreachable!();
         } else {
             asm!("
                     lea 1f(%rip), %rcx // set SGX AEP
